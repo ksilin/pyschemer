d3cb5c600af4172a014aa986585a9945eaaff06f
ksilin <konstantin.silin@gmail.com>
2024-07-29 12:30:10 +0200

    updated schema and repo

--- tests/si_sr_evo_test.py
+++ tests/si_sr_evo_test.py
@@ -11,7 +11,6 @@ from confluent_kafka import (
     SerializingProducer,
 )
 from confluent_kafka.admin import AdminClient, NewTopic
-from confluent_kafka.error import ValueSerializationError
 from confluent_kafka.schema_registry import (
     Schema,
     SchemaRegistryClient,
@@ -28,7 +27,7 @@ logger = logging.getLogger(__name__)
 
 KAFKA_BROKER = "localhost:39092"
 SCHEMA_REGISTRY_URL = "http://localhost:8081"
-TOPICS = ["si-topic-evo", "si-topic-evo-open"]
+TOPICS = ["si-topic-evo"]
 
 def load_json(json_file: str) -> str | None:
     json_str = None
@@ -135,26 +134,78 @@ def try_except(lambda_try, lambda_except, exception):
     except exception:
         return lambda_except()
 
+
+#def test_adding_new_property_with_open_model_is_backwards_compatible:
+    
+#def test_adding_new_property_with_closed_model_is_incompatible:
+    
+    
+#def test_changing_close_to_open_model_is_forwards_compatible:
+    
+#def test_sending_irrelevant_data_in_open_model_consumed:
+
+
 def test_produce_evo_closed():
 
-    #schema_stringV1 = json.dumps(schema_closed)
-    
-    producerV1 = create_test_producer(KAFKA_BROKER, SCHEMA_REGISTRY_URL, schema_str)
+    schema_v1_closed=json.loads(schema_str)
 
-    producerV1.produce('test-topic-evo', key='key1', value=json.loads(data), on_delivery=acked)
+    schema_v1_closed["additionalProperties"] = False
+
+    schema_v1_string = json.dumps(schema_v1_closed)
+
+    producerV1 = create_test_producer(KAFKA_BROKER, SCHEMA_REGISTRY_URL, schema_v1_string)
+
+    producerV1.produce('si-topic-evo', key='key1', value=json.loads(data), on_delivery=acked)
     producerV1.flush()
-    
-    consumer = create_test_consumer(KAFKA_BROKER, SCHEMA_REGISTRY_URL, schema_str, 'test-group', 'test-topic-evo')
+
+    consumer = create_test_consumer(KAFKA_BROKER, SCHEMA_REGISTRY_URL, schema_str, 'test-group', 'si-topic-evo')
     msg = consumer.poll(timeout=10.0)
 
-    print("consumed message:")
-    print(msg.value())
+    #print("consumed message:")
+    #assert msg is not None
+    #print(msg.value())
 
-    assert msg is not None
-    assert msg.key() == 'key1'
-    
     schema_registry_conf = {'url': SCHEMA_REGISTRY_URL}
     schema_registry_client = SchemaRegistryClient(schema_registry_conf)
-    
-    compat = try_except(lambda: schema_registry_client.get_compatibility(subject_name="test-topic-evo-value"), lambda: schema_registry_client.get_compatibility(), SchemaRegistryError)
+
+    #schema_registry_client.set_compatibility(subject_name="si-topic-evo-value", level="FORWARD")
+    compat = try_except(lambda: schema_registry_client.get_compatibility(subject_name="si-topic-evo-value"), lambda: schema_registry_client.get_compatibility(), SchemaRegistryError)
     print(f"compat: {compat}")
+    
+    # schema V2 anpassen
+    
+    schema_v2_obj=copy.deepcopy(schema_v1_closed)
+    
+    # additionalProperties = True is BACKWARDS compatible
+    schema_v2_obj["additionalProperties"] = True
+    
+    schema_v2_str = json.dumps(schema_v2_obj)
+    
+    schema_registry_client.test_compatibility(subject_name="si-topic-evo-value", schema=Schema(schema_v2_str, schema_type="JSON"), version="latest")
+    
+    #schema_v2_obj["new_attribute"] = {
+    #    "type": "string"
+    #  }
+    
+    added_attribute_compatible = schema_registry_client.test_compatibility(subject_name="si-topic-evo-value", schema=Schema(schema_v2_str, schema_type="JSON"), version="latest")
+    
+    print("added_attribute_compatible:")
+    print(added_attribute_compatible)
+    
+    
+    producerV2 = create_test_producer(KAFKA_BROKER, SCHEMA_REGISTRY_URL, schema_v2_str)
+    
+    data_v2_obj = json.loads(data)
+    data_v2_obj["new_attribute"] = "bazinga!"
+
+    producerV2.produce('si-topic-evo', key='key1', value=data_v2_obj, on_delivery=acked)
+    producerV2.flush()
+    
+    msg2 = consumer.poll(timeout=10.0)
+
+    print("consumed message:")
+    assert msg2 is not None
+    print(msg2.value())
+
+    # compatibilität prüfen
+    # mit schema v2 daten schreiben
